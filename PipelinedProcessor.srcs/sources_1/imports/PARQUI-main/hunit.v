5ca0afa6f967a4ca15c05aeea070dd1de62410e2
Maykol Morales Morante <maykol.morales.morante@gmail.com>
2024-11-28 19:57:40 -0500

    Commit

--- PipelinedProcessor.srcs/sources_1/imports/PARQUI-main/hunit.v
+++ PipelinedProcessor.srcs/sources_1/imports/PARQUI-main/hunit.v
@@ -1,9 +1,15 @@
 module hazard_unit(
-    input wire RegWriteM, RegWriteM
-    input wire [3:0] RA1E, RA2E, WA3M
+    input wire RegWriteM, RegWriteM, MemtoRegE,
+    input wire [3:0] RA1E, RA2E, WA3M,
     output wire Match_1E_M, Match_2E_M, Match_1E_W, Match_2E_W,
-    output wire ForwardAE
+    output wire ForwardAE,
+
+    input wire [3:0] RA1D, RA2D, WA3E,
+    output wire Match_12D_E,
+    output wire StallF, StallD, StallE, FlushE
 );
+    wire ldrstall;
+
     assign Match_1E_M = (RA1E == WA3M);
     assign Match_2E_M = (RA2E == WA3M);
 
@@ -19,4 +25,12 @@ module hazard_unit(
     else begin
         assign ForwardAE = 00;
     end
+
+    assign Match_12D_E = (RA1D == WA3E) | (RA2D == WA3E);
+    assign ldrstall = Match_12D_E & MemtoRegE;
+
+    assign StallF = ldrstall;
+    assign StallD = ldrstall;
+    assign StallE = ldrstall;
+    assign FlushE = ldrstall;
 endmodule
